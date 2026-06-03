ff03e76c3f7341fd7fe656592bb3659780dc9ea7
NicolasNegretto7 <nicolasnegretto@gmail.com>
2026-06-03 15:24:42 -0300

    main

--- Clases/Class 11 pero bien/script.js
+++ Clases/Class 11 pero bien/script.js
@@ -1,25 +1,60 @@
-
 const inputNombre = document.getElementById("inputNombre");
 const inputURL = document.getElementById("inputURL");
-const btnCrearImagen = document.getElementById("CrearImagen");
-const btnBorrarGaleria = document.getElementById("BorrarGaleria");
-const btnMostrarTodas = document.getElementById("MostrarTodas");
-const btnMostrarFavoritos = document.getElementById("MostrarFavoritos");
-const btnMostrarNoFavoritos = document.getElementById("MostrarNoFavoritos");
-
-const Total = document.getElementById("Total");
-const Favoritos = document.getElementById("Favoritos");
-const NoFavoritos = document.getElementById("NoFavoritos");
 const Galeria = document.getElementById("Galeria");
 
+const TotalInZero = document.getElementById("Total");
+const Favoritos = document.getElementById("Favoritos");
+const NoFavoritos = document.getElementById("NoFavoritos");
+
 function CrearNuevaImagen() {
+
+    if (inputNombre.value === "" || inputURL.value === "") {
+    alert("Error,porfavor rellene los formularios con nombre y URL de la imagen");
+
+    return;
+
+        }
+
     const contenedor = document.createElement("div");
+    contenedor.style.border = "1px solid black"
+    contenedor.style.margin = "10px";
+    contenedor.style.padding = "10px";
+    contenedor.style.display = "inline-block";
+    
     const img = document.createElement("img");
-    const titulo = document.createElement("titulo");
+    const titulo = document.createElement("p");
+    const Eliminar = document.createElement("button");
+    const Favoritos = document.createElement("button");
 
     img.src = inputURL.value;
     img.alt = inputNombre.value;
     img.width = 200;
+    titulo.textContent = inputNombre.value;
+    Eliminar.textContent = "Borrar";
+    Eliminar.type = "button";
+    Favoritos.textContent = "Favorito";
+    Favoritos.type = "button";
 
-    document.getElementById("Galeria").appendChild(img);
+    Eliminar.onclick = function() {
+    contenedor.remove(); 
+
+    };
+
+    Favoritos.onclick = function() {
+        contenedor.classList.toggle("favorito");
+    }
+
+    contenedor.appendChild(img);
+    contenedor.appendChild(titulo);
+    contenedor.appendChild(Eliminar);
+    contenedor.appendChild(Favoritos);
+    document.getElementById("Galeria").appendChild(contenedor);
+
+}
+
+
+function BorrarGaleria() {
+        if (confirm("¿Seguro que querés borrar toda la galería?")) {
+        Galeria.innerHTML = "";
+}
 }
