18fd4f57ab5831bdeff2dced6b85b1a50382aeab
NicolasNegretto7 <nicolasnegretto@gmail.com>
2026-06-10 13:49:24 -0300

    main

---
+++ Clases/Clase 13/script.js
@@ -0,0 +1,22 @@
+const btnChiste = document.getElementById("BotonChiste");
+const divChiste = document.getElementById("Chiste");
+
+btnChiste.addEventListener("click", async () => {
+
+    try {
+        const url = await fetch("https://sv443.net/jokeapi/v2/", {
+            method: "GET",
+            
+        }
+
+
+
+
+        )
+    }
+
+}
+
+
+
+);
