66a11c9d1c6cc06c18053862c1551c3d457d0b81
NicolasNegretto7 <nicolasnegretto@gmail.com>
2026-06-10 14:32:17 -0300

    main

--- Clases/Clase 13/script.js
+++ Clases/Clase 13/script.js
@@ -4,19 +4,21 @@ const divChiste = document.getElementById("Chiste");
 btnChiste.addEventListener("click", async () => {
 
     try {
-        const url = await fetch("https://sv443.net/jokeapi/v2/", {
+        const url = await fetch("https://v2.jokeapi.dev/joke/Any?type=single", {
             method: "GET",
-            
-        }
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
 
+        const data = await url.json();
 
-
-
-        )
+    if (data.type === "single") {
+      divChiste.textContent = data.joke;
+    } else {
+      divChiste.textContent = data.setup + " ... " + data.delivery;
     }
-
-}
-
-
-
-);
+  } catch (error) {                                            
+    console.error("Error:", error);
+  }
+});
