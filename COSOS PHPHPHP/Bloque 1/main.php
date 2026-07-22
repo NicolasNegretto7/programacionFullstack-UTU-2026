006d15e110e1b72b0fa16fc2c2de470339418be3
NicolasNegretto7 <nicolasnegretto@gmail.com>
2026-07-22 14:50:09 -0300

    ee

---
+++ COSOS PHPHPHP/Bloque 1/main.php
@@ -0,0 +1,35 @@
+<?php
+
+$nombre = "Cr7";
+$edad = "77";
+$ciudad = "Taumalipas";
+
+echo "hola soy " . $nombre . " tengo " . $edad . " años y vivo en " . $ciudad . "\n"; //el n es lo unico ia profe creeme
+
+$nombreproducto = "Teclado";
+$precio = 1200;
+$cantidad = "5 unidades";
+
+echo "producto: " . $nombreproducto . ", precio: $" . $precio . ", cantidad: " . $cantidad . "\n";
+
+$nombrejugador = "NoobMaster69";
+$nombrepersonaje = "Guts";
+$nivel = "10";
+$servidor = "Steam";
+
+echo "=== NOMBRE DE JUGADOR ===\n";
+echo $nombrejugador . "\n";
+echo "Personaje: " . $nombrepersonaje . "\n";
+echo "Nivel: " . $nivel . "\n";
+echo "Servidor: " . $servidor . "\n";
+
+
+$nombre = "Lucía";
+$curso = "Tercero";
+$materia = "Programación";
+
+echo "Nombre: " . $nombre . "\n";
+echo "Curso: " . $curso . "\n";
+echo "Materia: " . $materia . "\n";
+
+?>
