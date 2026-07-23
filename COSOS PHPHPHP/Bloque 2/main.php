960f4677a0414ecba08056816091bbb3d7d3aace
NicolasNegretto7 <nicolasnegretto@gmail.com>
2026-07-23 16:29:55 -0300

    e

--- COSOS PHPHPHP/Bloque 2/main.php
+++ COSOS PHPHPHP/Bloque 2/main.php
@@ -1,5 +1,37 @@
 <?php
 
+$numero1 = 1;
+$numero2 = 2;
+
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$multiplicacion = $numero1 * $numero2;
+$division = $numero1 / $numero2;
+
+echo "suma: " . $suma . "\n";
+echo "resta: " . $resta . "\n";
+echo "multiplicacion: " . $multiplicacion . "\n";
+echo "division: " . $division . "\n" . "\n";
+
+$base = 30;
+$altura = 200;
+
+$area = $base * $altura;
+
+echo "el area del triangulo es: " . $area . " \n" . "\n";
+
+$nota1 = 8;
+$nota2 = 7;
+$nota3 = 9;
+
+$promedio = ($nota1 + $nota2 + $nota3) / 3;
+
+echo "nota 1: " . $nota1 . "\n";
+echo "nota 2: " . $nota2 . "\n";
+echo "nota 3: " . $nota3 . "\n";
+echo "nota promedio: " . $promedio . "\n" . "\n";
+
+
 
 
 ?>
